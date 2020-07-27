28de87de750d8e5fd1874105a624f27f55f5cf25
Adam Jochemczyk <adamjoche@gmail.com>
2020-07-27 09:00:23 +0200

    filters in reserve cars

--- klient/src/Components/ReserveCar/ReserveCar.jsx
+++ klient/src/Components/ReserveCar/ReserveCar.jsx
@@ -1,19 +1,26 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CardCar from "../CardCar/CardCar";
-import { Col, Form, Input, Row, Button } from "reactstrap";
+import { Col, Form, Input, Row } from "reactstrap";
 import Loader from "react-loader-spinner";
 import Swal from "sweetalert2";
 import { useHistory } from "react-router-dom";
 
 export default function ReserveCar() {
   const [data, setData] = useState([]);
-  const [filters, setFilter] = useState({});
+  const [filters, setFilter] = useState({
+    brand: "",
+    model: "",
+    registrationNumber: "",
+    yearOfProduction: "",
+    numberOfDoor: "",
+    numberOfSits: ""
+  });
   const [isLoading, setIsLoading] = useState(false);
   const history = useHistory();
 
   useEffect(() => {
-    async function fetchCars(){
+    async function fetchCars() {
       try {
         setIsLoading(true);
         const response = await axios.get("https://localhost:44390/api/cars");
@@ -34,16 +41,7 @@ export default function ReserveCar() {
       [event.target.name]: event.target.value,
     });
   }
-
-  function reloadDataWithFilters() {
-    console.log("clicked");
-    let filteredData = {
-      carId: 55,
-      brand: "test",
-    };
-    setData(filteredData);
-  }
-
+  console.log(data)
   function CreateCarCard(data) {
     return (
       <CardCar
@@ -59,6 +57,7 @@ export default function ReserveCar() {
       />
     );
   }
+
   return (
     <div>
       {isLoading ? (
@@ -67,22 +66,71 @@ export default function ReserveCar() {
         </div>
       ) : (
         <div>
-          <Col sm={2} style={{ float: "left", textAlign: "center" }}>
-            <Form>
-              Search your car by:
-              <Input
-                type="text"
-                placeholder="Brand"
-                name="brand"
-                value={filters.brand}
-                onChange={handleChange}
-              />
-              <Button onClick={reloadDataWithFilters}>Click me!</Button>
-            </Form>
-          </Col>
-          <Col>
-            <Row>{data.map(CreateCarCard)}</Row>
-          </Col>
+          <Row>
+            <Col sm={2}>
+              <Form>
+                Search your car by:
+                <Input
+                  type="text"
+                  placeholder="Brand"
+                  name="brand"
+                  value={filters.brand}
+                  onChange={handleChange}
+                />
+                <Input
+                  type="text"
+                  placeholder="Model"
+                  name="model"
+                  value={filters.model}
+                  onChange={handleChange}
+                />
+                <Input
+                  type="text"
+                  placeholder="Registration Number"
+                  name="registrationNumber"
+                  value={filters.registrationNumber}
+                  onChange={handleChange}
+                />
+                <Input
+                  type="text"
+                  placeholder="Year Of Production"
+                  name="yearOfProduction"
+                  value={filters.yearOfProduction}
+                  onChange={handleChange}
+                />
+                <Input
+                  type="text"
+                  placeholder="Number of doors"
+                  name="numberOfDoor"
+                  value={filters.numberOfDoor}
+                  onChange={handleChange}
+                />
+                <Input
+                  type="text"
+                  placeholder="Sits places"
+                  name="numberOfSits"
+                  value={filters.numberOfSits}
+                  onChange={handleChange}
+                />
+              </Form>
+            </Col>
+            <Col>
+              <Row>
+                {data
+                  .filter((data) => {
+                    return (
+                      data.brand.toLowerCase().includes(filters.brand.toLowerCase()) &&
+                      data.model.toLowerCase().includes(filters.model.toLowerCase()) &&
+                      data.registrationNumber.toLowerCase().includes(filters.registrationNumber.toLowerCase()) &&
+                      data.yearOfProduction >=filters.yearOfProduction &&
+                      data.numberOfDoor >=filters.numberOfDoor &&
+                      data.numberOfSits >= filters.numberOfSits
+                    );
+                  })
+                  .map(CreateCarCard)}
+              </Row>
+            </Col>
+          </Row>
         </div>
       )}
     </div>
